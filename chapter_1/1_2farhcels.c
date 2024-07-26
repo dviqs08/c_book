4aa36ba378097a03b4f2e1b03af38dfc0dcbd11d
dviqs08 <115051889+dviqs08@users.noreply.github.com>
2024-07-26 15:16:21 -0400

    updated to use floating point

--- chapter_1/1_2farhcels.c
+++ chapter_1/1_2farhcels.c
@@ -5,8 +5,8 @@
 
 int main() 
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float  lower, upper, step;
 
   lower = 0;
   upper = 300;
@@ -14,8 +14,8 @@ int main()
 
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
  
